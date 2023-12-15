3a152b561d98d6e2671f49c4f724d1b509d9b767
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-15 21:29:51 +0700

    Update oauth.controller.ts

--- src/controllers/oauth/oauth.controller.ts
+++ src/controllers/oauth/oauth.controller.ts
@@ -1,1 +1,11 @@
-console.log('ok')
+
+import { OAuth2Client } from 'google-auth-library'
+import { Controller,Get } '@nestjs/common';
+
+
+@Controller('oauth') 
+export class UserController {
+  @Get('authorize/google') authorizeGoogle():string{
+    return 'url'
+  }
+}

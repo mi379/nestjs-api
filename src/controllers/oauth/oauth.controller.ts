091e9d8806f481c5243469b3492672ff114acb69
mkzbdfgh@gmail.com <mkzbdfgh@gmail.com>
2023-12-16 03:02:58 +0700

    update oauth.controller.ts

--- src/controllers/oauth/oauth.controller.ts
+++ src/controllers/oauth/oauth.controller.ts
@@ -6,6 +6,15 @@ import { Controller,Get,Query } from '@nestjs/common';
 @Controller('oauth') 
   
 export class OauthController {
+  
+  redirect:string = 'https://nestjs-api-production-f720.up.railway.app/oauth/authorize/google/callback'
+  infoUrl:string = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json'
+  
+  oAuth2Client:OAuth2Client = new OAuth2Client(
+    process.env.CLIENT_ID, 
+    process.env.CLIENT_SECRET, 
+    this.redirect
+  )
 
   @Get('authorize/google') 
   
@@ -13,36 +22,24 @@ export class OauthController {
     var googleApi:string = 'https://www.googleapis.com'
     var profile:string = `${googleApi}/auth/userinfo.profile`
     var email:string = `${googleApi}/auth/userinfo.email`
-   
-    var oauth2Client:OAuth2Client = new OAuth2Client(
-      process.env.CLIENT_ID, 
-      process.env.CLIENT_SECRET, 
-      'https://nestjs-api-production-f720.up.railway.app/oauth/authorize/google/callback'
-    )
-   
-    return oauth2Client.generateAuthUrl({
+    
+    return this.oauth2Client.generateAuthUrl({
       access_type:'offline',
       scope:[email,profile]
     });
   }
   
+  
   @Get('authorize/google/callback')
   
-   async authorizeGoogleCallback(@Query('code') code:string): Promise<any> {
-     var oauth2Client: OAuth2Client = new OAuth2Client(
-       process.env.CLIENT_ID,
-       process.env.CLIENT_SECRET,
-       'https://nestjs-api-production-f720.up.railway.app/oauth/authorize/google/callback'
-     )
+   async googleAuthorizationCallback(@Query('code') code:string): Promise<any> {
+     var {tokens}:{tokens:string} = await this.oauth2Client.getToken(code)
      
-     var x = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json'
+     var credential = this.oAuth2Client.setCredentials(tokens)
      
-     var authenticated = await oauth2Client.getToken(code)
-     
-     oauth2Client.setCredentials(authenticated.tokens)
-     
-     var info = await oauth2Client.request({url:x})
+     var info = await oauth2Client.request({url:this.infoUrl})
      
      return info
    }
 }
+

15856024e4a5e56bf7ab76d871d211fc8d1921d4
mkzbdfgh@gmail.com <mkzbdfgh@gmail.com>
2023-12-16 01:10:24 +0700

    update oauth.controller.ts

--- src/controllers/oauth/oauth.controller.ts
+++ src/controllers/oauth/oauth.controller.ts
@@ -10,10 +10,10 @@ export class OauthController {
   @Get('authorize/google') 
   
   authorizeWithGoogle():string{
-    var authApi:string = 'https://googleapis.com/auth'
-    var email:string   = `${authApi}/userinfo.email`
-    var profile:string = `${authApi}/userinfo.profile`
-    
+    var googleApi:string = 'https://www.googleapis.com'
+    var profile:string   = `${googleApi}/userinfo.profile`
+    var email:string     = `${googleApi}/auth/userinfo.email`
+   
     var oauth2Client:OAuth2Client = new OAuth2Client(
       process.env.CLIENT_ID, 
       process.env.CLIENT_SECRET, 
@@ -22,7 +22,7 @@ export class OauthController {
    
     return oauth2Client.generateAuthUrl({
       access_type:'offline',
-      scope:`${authApi}/userinfo.email`
+      scope:[email,profile]
     });
   }
 }

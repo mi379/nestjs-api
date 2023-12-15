b799b0dbfeb25b45b82fc122b110a22ae984c4a3
mkzbdfgh@gmail.com <mkzbdfgh@gmail.com>
2023-12-16 01:29:19 +0700

    update oauth.controller.ts

--- src/controllers/oauth/oauth.controller.ts
+++ src/controllers/oauth/oauth.controller.ts
@@ -25,4 +25,20 @@ export class OauthController {
       scope:[email,profile]
     });
   }
+  
+  @Get('authorize/google/callback')
+  
+   async authorizeGoogleCallback(): Promise<any> {
+     var oauth2Client: OAuth2Client = new OAuth2Client(
+       process.env.CLIENT_ID,
+       process.env.CLIENT_SECRET,
+       process.env.REDIRECT
+     )
+    
+     var code = '4%2F0AfJohXkDPwqHRJG0GPW0h1ca-Ydbit4urYX8vzHOw2PZSZlGbPjCRoMbRh2UH7gy3UD2dw'
+     var {token} = await oauth2Client.getToken(code as string)
+     
+     
+     return token
+   }
 }

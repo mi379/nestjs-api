b85b19fa766986feaffe8a1c75fd4ca0bbd7d5a2
mkzbdfgh@gmail.com <mkzbdfgh@gmail.com>
2023-12-16 00:50:10 +0700

    update oauth.controller.ts

--- src/controllers/oauth/oauth.controller.ts
+++ src/controllers/oauth/oauth.controller.ts
@@ -6,7 +6,23 @@ import { Controller,Get } from '@nestjs/common';
 @Controller('oauth') 
   
 export class OauthController {
-  @Get('authorize/google') authorizeGoogle():string{
-    return 'url'
+
+  @Get('authorize/google') 
+  
+  authorizeWithGoogle():string{
+    var authApi:string = 'https://googleapis.com/auth'
+    var email:string   = `${authApi}/userinfo.email`
+    var profile:string = `${apiUrl}/userinfo.profile`
+    
+    var oauth2Client:OAuth2Client = new OAuth2Client(
+      process.env.CLIENT_ID, 
+      process.env.CLIENT_SECRET, 
+      process.env.REDIRECT
+    )
+   
+    return oauth2Client.generateAuthUrl({
+      access_type:'offline',
+      scope:[email,profile]
+    });
   }
 }

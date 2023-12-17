315796fe27d8290e9cc6eb75d287b736ec723f19
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-17 08:35:16 +0700

    Update oauth.controller.ts

--- src/controllers/oauth/oauth.controller.ts
+++ src/controllers/oauth/oauth.controller.ts
@@ -60,7 +60,7 @@ export class OauthController {
          ) 
 
          var profile = await this.profileSvc({
-           firstName:data.familyName, 
+           firstName:data.family_name, 
            surname:data.given_name, 
            profileImage:data.picture, 
            usersRef:user._id

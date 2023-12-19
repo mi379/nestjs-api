8d60389e750f567dc073bd2dc12f3dc3d32ee9c1
Muhammad Iqbal <107986705+mi379@users.noreply.github.com>
2023-12-19 07:30:08 +0700

    Update oauth.dto.ts

--- src/dto/oauth.dto.ts
+++ src/dto/oauth.dto.ts
@@ -1,1 +1,13 @@
+import { IsNotEmpty } from 'class-validator'
 
+export class LoginDto{
+  @IsNotEmpty() oauthReference:string
+  
+  @IsNotEmpty() profileImage:string
+  
+  @IsNotEmpty() firstName:string
+
+  @IsNotEmpty() surname : string
+
+  
+}
